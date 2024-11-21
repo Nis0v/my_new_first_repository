7ae93f5954771e1611289a25afa5ef80a69a1a84
Evgeniy Nisov <nisov.03@mail.ru>
2024-11-21 21:03:12 +0300

    new3

--- some_file.py
+++ some_file.py
@@ -1,2 +1,13 @@
-print("number 1")
-print("number 2")
+import requests
+
+import requests
+
+
+api_url = 'http://numbersapi.com/43'
+
+response = requests.get(api_url)  # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:  # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+else:
+    print(response.status_code)  # При другом коде ответа выводим этот код
